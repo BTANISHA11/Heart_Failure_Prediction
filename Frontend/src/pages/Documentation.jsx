799f95835746084c5c197d56da1826005c70f066
TANISHA BANSAL <tanishab130@gmail.com>
2023-08-27 18:19:12 +0530

    frontend

--- Frontend/src/pages/Documentation.jsx
+++ Frontend/src/pages/Documentation.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-
 export default function Documentation() {
+  // const driveLink = "https://drive.google.com/file/d/1TFMw8NpEJ4ICCO4MkeTmUcA2HYIp1qMt/view?usp=sharing";
+// export default function Documentation() {
   return (
     <div>
       <Navbar />
@@ -9,6 +10,9 @@ export default function Documentation() {
         <iframe
           className="doc"
           src="https://research-paper.tiiny.site/"
+          // src="ML\HEART FAILURE PREDICTION.ipynb"
+          // src="HEART FAILURE PREDICTION.html"
+          // src={driveLink}
           width="1700"
           height="850"
           // margin=""
