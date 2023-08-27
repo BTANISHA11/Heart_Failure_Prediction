3b39d8b7ec6b100c55b6d6ae5d13539269db126b
TANISHA BANSAL <tanishab130@gmail.com>
2023-08-28 00:13:36 +0530

    analyse

--- Frontend/src/pages/Analysis.jsx
+++ Frontend/src/pages/Analysis.jsx
@@ -2,32 +2,24 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from "recharts";
 
-const HeartFailureAnalysis = () => {
-  // Sample analysis data for heart failure prediction (replace with your actual data)
-  const data = [
-    { category: "Age", survived: 50, notSurvived: 100 },
-    { category: "Gender", survived: 70, notSurvived: 80 },
-    { category: "SmokingStatus", survived: 30, notSurvived: 120 },
-    { category: "Diabetes", survived: 30, notSurvived: 120 },
-    { category: "Anameia", survived: 30, notSurvived: 120 },
-    { category: "HighBloodPressure", survived: 30, notSurvived: 120 },
-    // Add more data...
-  ];
+import heartImage from "../Assets/age_graph1.png"; // Replace with the actual path to your image
+import heartImage1 from "../Assets/smoker.png";
+import heartImage2 from "../Assets/diabetes.png";
+import heartImage3 from "../Assets/anaemia.png";
 
+
+const HeartFailureAnalysis = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <Navbar />
       <div className="analysis">
         <h2 style={{ textAlign: "center" }}>Heart Failure Prediction Analysis</h2>
         <div style={{ textAlign: "center" }}>
-          <BarChart width={600} height={400} data={data}>
-            <XAxis dataKey="category" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="survived" fill="#8884d8" name="Survived" />
-            <Bar dataKey="notSurvived" fill="#82ca9d" name="NotSurvived" />
-          </BarChart>
+          <img src={heartImage} alt="Heart" style={{ maxWidth: "100%", height: "auto" }} />
+          <img src={heartImage1} alt="Heart" style={{ maxWidth: "100%", height: "auto" }} />
+          <img src={heartImage2} alt="Heart" style={{ maxWidth: "100%", height: "auto" }} />
+          <img src={heartImage3} alt="Heart" style={{ maxWidth: "100%", height: "auto" }} />
+
         </div>
       </div>
     </div>
